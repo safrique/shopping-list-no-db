34991009c1a266b85e4e83d3c91581efe0ce70ab
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-04-16 15:43:06 +0100

    made items move to another list depending on checkbox checked status

--- src/scripts/db.js
+++ src/scripts/db.js
@@ -8,8 +8,8 @@ window.onload = () => {
 }
 
 function displayLists () {
-  console.log(localStorage)
-  console.log(localStorage.length)
+  // console.log(localStorage)
+  // console.log(localStorage.length)
 
   let outs = document.getElementById(`outs_items`)
   let comp = document.getElementById(`comp_items`)
@@ -21,9 +21,11 @@ function displayLists () {
       let st = document.createElement(`span`)
       let cb = document.createElement(`input`)
 
+      li.id = `li_${key}`
       cb.id = `cb_${key}`
       cb.type = `checkbox`
-      st.id = `span_${key}`
+      cb.onchange = () => itemStatusChanged(cb)
+      // st.id = `span_${key}`
       st.innerHTML = key
 
       s.appendChild(cb)
@@ -42,6 +44,25 @@ function displayLists () {
     }
   }
 
-  console.log(outs)
-  console.log(comp)
+  // console.log(outs)
+  // console.log(comp)
+}
+
+function itemStatusChanged (cb) {
+  let li = document.getElementById(cb.id.replace(`cb`, `li`))
+  let outs = document.getElementById(`outs_items`)
+  let comp = document.getElementById(`comp_items`)
+  // console.log(outs)
+  // console.log(comp)
+  // console.log(cb)
+  // console.log(`${cb.id}=${cb.checked}`)
+  // console.log(li)
+
+  if (cb.checked) {
+    outs.removeChild(li)
+    comp.appendChild(li)
+  } else {
+    comp.removeChild(li)
+    outs.appendChild(li)
+  }
 }
